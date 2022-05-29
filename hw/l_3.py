2f47145a795282b119a9c4b202033f630b1a90fc
evtye <evtye32@gmail.com>
2022-05-29 15:39:13 +0200

    sss

--- hw/l_3.py
+++ hw/l_3.py
@@ -6,7 +6,7 @@ from requests import Response
 class TestHW3:
     @pytest.mark.skip('#1')
     def test_phrase(self):
-        phrase = 'eblan'
+        phrase = 'dfggg'
         assert len(phrase) < 15, "Длина не меньше 15 символов"
 
     @pytest.mark.skip('#2')
